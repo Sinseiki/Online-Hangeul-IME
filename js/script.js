7a9c2775e24d60118a479dbcd90d03a496aa7199
sinseiki <eekdland@naver.com>
2020-02-15 01:51:31 +0900

    웹브라우저에서 직접 읽는 파일들을 압축함

--- js/script.js
+++ js/script.js
@@ -1,26 +1,4 @@
-show_keyboard_layout(1);
-inputText_focus();
-// 탭 제어부
-function gotoTab(n) {
-    ohi_menu_num = n;
-    for (var i = 1; i <= 3; i++) {
-        $('#menu' + i)[0].className = ((i === n) ? 'this' : 'menu');
-        $('#tab' + i).css('height', (i === n) ? 'auto' : '1px');
-        //$('#tab'+i).css('position',(i==n) ? 'static' : 'absolute');
-        $('#tab' + i).css('overflow', (i === n) ? 'visible' : 'hidden');
-        $('#tab' + i).css('visibility', (i === n) ? 'visible' : 'hidden');
-    }
-    $('#manual').css('display', (n != 3) ? 'block' : 'none');
-    show_keyboard_layout();
-}
-$('.left_menu div').click(function () { gotoTab(Number($(this).attr('id').charAt(4))); });
-// 한·영 상태 제어부
-$('.korEngStatus').click(function () { ohiChange_KE(); inputText_focus(); });
-// 기준 자판 제어부
-function ruleKBD(x) {
-    $('.ruleKBD' + x).click(function () { ohiChange_KBD_type(x); inputText_focus(); });
-}
-ruleKBD('');
-ruleKBD('QWERTY');
-ruleKBD('QWERTZ');
-ruleKBD('AZERTY');
+show_keyboard_layout(1);inputText_focus();function gotoTab(n){ohi_menu_num=n;for(var i=1;i<=3;i++){$('#menu'+i)[0].className=((i===n)?'this':'menu');$('#tab'+i).css('height',(i===n)?'auto':'1px');$('#tab'+i).css('overflow',(i===n)?'visible':'hidden');$('#tab'+i).css('visibility',(i===n)?'visible':'hidden')}
+$('#manual').css('display',(n!=3)?'block':'none');show_keyboard_layout()}
+$('.left_menu div').click(function(){gotoTab(Number($(this).attr('id').charAt(4)))});$('.korEngStatus').click(function(){ohiChange_KE();inputText_focus()});function ruleKBD(x){$('.ruleKBD'+x).click(function(){ohiChange_KBD_type(x);inputText_focus()})}
+ruleKBD('');ruleKBD('QWERTY');ruleKBD('QWERTZ');ruleKBD('AZERTY')
